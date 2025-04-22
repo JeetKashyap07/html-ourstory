225bbadd1eaab81da5b1cff039b1eee1ec8956c3
JeetKashyap07 <kanwarjeetkashyaap@gmail.com>
2025-04-22 16:25:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("future").addEventListener("click", function() {
+    alert("Our future is filled with endless possibilities. We will make it through, no matter the distance.");
+  });
+  
+
+
+
+
+
